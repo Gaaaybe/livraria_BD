beb44d4a519ce3cc3b2743d1fd999eb3f77dd742
Gaaaybe <gabenesus@gmail.com>
2024-10-28 18:38:08 -0300

    feat(logger): remove error log file

--- src/config/logger.js
+++ src/config/logger.js
@@ -4,7 +4,6 @@ const logger = winston.createLogger({
   level: 'info',
   format: winston.format.json(),
   transports: [
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
     new winston.transports.File({ filename: 'info.log'}),
   ],
 });
