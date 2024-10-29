8fdb1e9550feee6e10d83d6a2fc1b34636d43f7c
Gaaaybe <gabenesus@gmail.com>
2024-10-28 22:41:31 -0300

    fix(server): atualiza porta para 3001

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import "dotenv/config";
 import app from "./src/app.js";
 import logger from "./src/config/logger.js";
 
-const PORT = 3000;
+const PORT = 3001;
 
 app.listen(PORT, () => {
     logger.log('info', `Servidor rodando no link http://localhost:${PORT}/`);
