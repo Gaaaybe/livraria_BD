e166c582b3d480e1a32e0ab03185cbc4bfec0ab9
Gabriel Menezes <gabenesus@gmail.com>
2024-10-30 10:11:07 -0300

    feat(logger): adiciona transporte para registrar erros em error.log

--- src/config/logger.js
+++ src/config/logger.js
@@ -5,6 +5,7 @@ const logger = winston.createLogger({
   format: winston.format.json(),
   transports: [
     new winston.transports.File({ filename: 'info.log'}),
+    new winston.transports.File({ filename: 'error.log', level: 'error'}),
   ],
 });
 
